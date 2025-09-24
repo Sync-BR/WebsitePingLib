4baac6ed49098704328efdcf57092f589c148182
Eduardo <eduardofreitasfr@gmail.com>
2025-09-23 22:32:44 -0300

    feat: fix final domain

--- src/main/java/com/github/sync/ping/Ping.java
+++ src/main/java/com/github/sync/ping/Ping.java
@@ -1,6 +1,7 @@
 package com.github.sync.ping;
 
 import com.github.sync.ping.handle.HttpRequestException;
+import com.github.sync.ping.util.DomainIdentify;
 import com.github.sync.ping.util.FixUrl;
 
 import java.io.IOException;
@@ -8,25 +9,23 @@ import java.net.InetAddress;
 
 public class Ping {
     public FixUrl urlFix;
+    public DomainIdentify identify;
 
-    public boolean checkWebSite(String hostUrl){
+    public boolean checkWebSite(String hostUrl) {
         try {
             urlFix = new FixUrl();
-            InetAddress address = InetAddress.getByName(urlFix.checkPattern(hostUrl));
+            this.identify = new DomainIdentify();
+            InetAddress address = InetAddress.getByName(identify.fixUrl(urlFix.checkPattern(hostUrl)));
             if (address.isReachable(5000)) {
-               return true;
+                return true;
             } else {
                 throw new HttpRequestException("Erro ao tentar acessar o site: não foi possível completar a requisição. Verifique se o site está no ar ou se há problemas de rede.");
             }
-        }  catch (IOException e) {
+        } catch (IOException e) {
             throw new HttpRequestException("Erro ao tentar acessar o site: não foi possível completar a requisição. Verifique se o site está no ar ou se há problemas de rede.");
         }
     }
 
-    public static void main(String[] args) {
-        Ping ping = new Ping();
-        ping.checkWebSite("https://www.google.com");
-    }
 
 
 }
---
+++ src/main/java/com/github/sync/ping/enumeration/Domain.java
@@ -0,0 +1,5 @@
+package com.github.sync.ping.enumeration;
+
+public enum Domain {
+    _COM, _COM_BR, _BR, _NET, _ORG, _BET, _GG
+}
